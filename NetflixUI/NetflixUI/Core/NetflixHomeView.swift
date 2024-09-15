cc825ed289c210f26beef8590adbba253a4ad844
Aashish Katiyar <aashishkatiyar20@gmail.com>
2024-09-15 22:09:55 +0530

    Commit Home Header Change continue...

--- NetflixUI/NetflixUI/Core/NetflixHomeView.swift
+++ NetflixUI/NetflixUI/Core/NetflixHomeView.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct NetflixHomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            VStack {
+                Header
+                    .padding(.horizontal, 16)
+                
+                Spacer()
+            }
+        }
     }
 }
 
 #Preview {
     NetflixHomeView()
+}
+
+private var Header: some View {
+        HStack {
+            Text("To Netflix")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+            
+            HStack {
+                Image(systemName: "tv.badge.wifi")
+                    .onTapGesture {
+                        
+                    }
+                Image(systemName: "magnifyingglass")
+                    .onTapGesture {
+                        
+                    }
+            }
+            .font(.title2)
+        }
+        .foregroundColor(.netflixWhite)
 }
