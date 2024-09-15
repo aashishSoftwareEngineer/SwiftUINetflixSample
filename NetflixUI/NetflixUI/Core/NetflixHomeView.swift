1305a2e6732a0dbbb92b3726d3a40ab1ec93fc05
Aashish Katiyar <aashishkatiyar20@gmail.com>
2024-09-15 22:08:04 +0530

    Home Page create header part-1

---
+++ NetflixUI/NetflixUI/Core/NetflixHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixHomeView.swift
+//  NetflixUI
+//
+//  Created by Aashish Katiyar on 15/09/24.
+//
+
+import SwiftUI
+
+struct NetflixHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixHomeView()
+}
