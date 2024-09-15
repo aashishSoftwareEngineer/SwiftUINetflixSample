f454ea62cbace776a37d7dd0a9b99ad3662071e1
Aashish Katiyar <aashishkatiyar20@gmail.com>
2024-09-15 20:23:14 +0530

    Project Created

---
+++ NetflixUI/NetflixUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetflixUI
+//
+//  Created by Aashish Katiyar on 15/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetflixUI/NetflixUI/NetflixUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIApp.swift
+//  NetflixUI
+//
+//  Created by Aashish Katiyar on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
