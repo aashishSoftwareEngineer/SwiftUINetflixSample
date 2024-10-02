ac81728a1b3ffcac69b7d77e0008ffcabf30bfcc
Aashish Katiyar <aashishkatiyar20@gmail.com>
2024-10-02 11:04:06 +0530

    Filter cell UI Created.

---
+++ NetflixUI/NetflixUI/Components/NetflixFilterCell.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixFilterCell.swift
+//  NetflixUI
+//
+//  Created by Aashish Katiyar on 02/10/24.
+//
+
+import SwiftUI
+
+struct NetflixFilterCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixFilterCell()
+}
